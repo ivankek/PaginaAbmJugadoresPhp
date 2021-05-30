97ea181ea9ac54c9fc8effbc7c8a5f75b7d93494
ivankek <ivan.kek@hotmail.com>
2021-05-30 19:58:31 -0300

    Modal agregar jugador

--- jugadores.php
+++ jugadores.php
@@ -50,10 +50,10 @@
     <!--Agregar jugador botón-->
     <?php
 
-    if(isset($_SESSION["nombre"])){
+    if (isset($_SESSION["nombre"])) {
 
         echo "<div class='d-grid gap-2 col-6 mx-auto mt-5'>
-            <button class='btn btn-secondary' type='button' data-bs-toggle='modal' data-bs-target='#crearPokemon'>
+            <button class='btn btn-secondary' type='button' data-bs-toggle='modal' data-bs-target='#crearJugador'>
                 Agregar Jugador <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-plus' viewBox='0 0 16 16'>
   <path d='M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z'/>
 </svg></button>
@@ -65,9 +65,79 @@
 
     <!--Fin agregar jugador botón-->
 
+    <!--Modal agregar jugador-->
+
+    <div class="modal fade" id="crearJugador" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <form action="agregarJugador.php" method="POST" enctype="multipart/form-data">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Agregar un pokemon</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+
+                        <input class="mb-2 mb-md-0 mb-lg-0 mb-xl-0 mb-xxl-0" name="numero" type="number"
+                               placeholder="Número del jugador">
+                        <input class="" name="nombre" type="text" placeholder="Nombre del jugador">
+
+                        <div class="mt-3 mb-1">¿En que posición juega?</div>
+
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="posicion" id="arquero" value="arquero">
+                            <label class="form-check-label" for="inlineCheckbox1">Arquero</label>
+                        </div>
+
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="posicion" id="defensor" value="defensor">
+                            <label class="form-check-label" for="inlineCheckbox1">Defensor</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="posicion" id="mediocampista"
+                                   value="mediocampista">
+                            <label class="form-check-label" for="inlineCheckbox1">Mediocampista</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="posicion" id="delantero"
+                                   value="delantero">
+                            <label class="form-check-label" for="inlineCheckbox1">Delantero</label>
+                        </div>
+
+                        <br>
+                        <br>
+
+                        <div class="mb-3">
+                            <label for="validationTextarea" class="form-label">Descripción del jugador:</label>
+                            <textarea class="form-control " name="descripcion" id="descripcion"
+                                      placeholder="Required example textarea"></textarea>
+                            <div class="invalid-feedback">
+                                Descripcion
+                            </div>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="validationTextarea" class="form-label">Foto del jugador</label>
+                            <input type="file" name="file" class="form-control" aria-label="file example" required>
+                            <div class="invalid-feedback">Example invalid form file feedback</div>
+                        </div>
+
+
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cerrar</button>
+                        <button class="  fw-bolder btn btn-primary" type="submit" value="loguear">Agregar Jugador
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!--Final modal agregar jugador-->
+
+
     <!-- Div container lista de jugadores -->
     <div class="container d-flex rounded flex-row mt-4">
-
 
 
         <!-- No hay resultados -->
