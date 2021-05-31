5f0f881af130aee164474675128311442e5fa62b
ivankek <ivan.kek@hotmail.com>
2021-05-31 02:38:28 -0300

    Arreglos Css

--- mostrarJugador.php
+++ mostrarJugador.php
@@ -35,48 +35,37 @@ while ($fila = mysqli_fetch_array($result)) {
     }
 
 
-    /*echo "<div style='margin-left: 2.3em;' '>";
+    echo "<div class='ms-4 ms-lg-3 mt-4'>";
     echo '<div class="card" style="width: 15rem;">';
     echo "<img src='$imgMostrar' class='card-img-top' alt='$nombre'>";
     echo '<div class="card-body">';
-    echo '<h5 class="card-title">Card title</h5>';
-    echo '<p class="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>';
-    echo '<a href="#" class="btn btn-primary">Go somewhere</a>';
-    echo '</div>';
-    echo '</div>';
-    echo "</div>";*/
-
-    echo '<div id="tarjetaJugador" class="bg-light p-2 col-12 col-md-5 col-lg-3 d-flex me-4 mt-4 flex-wrap">';
-
-    echo "<div class='me-4'>
-         <img id='imgJugador' class='rounded-circle border' src='$imgMostrar'>
-     </div>";
-
-    echo '<div class="d-flex flex-column me-2">';
-
-    echo '<div class="d-flex">
-             <p class="text-dark mb-0">' . $numero . '&nbsp-&nbsp</p>
-             <p class="text-dark fw-bolder mb-0">' . $nombre . '</p>
-         </div>';
-
-    echo "<img class='mt-2' style='width: 2em' src='$imgMostrarPosicion'>";
-
-
-    echo '<a class="mt-3 bg-dark text-decoration-none text-center text-light rounded">Perfil</a>';
-
+    echo '<div class="d-flex">';
+    echo "<h5 class='card-title text-primary'>$numero</h5>";
+    echo "<h5 class='card-title'>&nbsp-&nbsp$nombre</h5>";
     if (isset($_SESSION["nombre"])) {
+        echo '<div class="dropdown position-absolute end-0 me-2">';
 
-        echo '<a class="mt-2 bg-dark text-decoration-none text-center text-light rounded">Editar</a>';
-        echo "<a href='borrarJugador.php?id=$numero' class='mt-2 bg-danger text-decoration-none text-center text-light rounded'>Eliminar</a>";
+        echo "<a href='#' role='button' id='dropdownMenuLink' data-bs-toggle='dropdown' aria-expanded='false'><svg xmlns='http://www.w3.org/2000/svg' width='25' height='25' fill='currentColor' class='bi bi-three-dots-vertical' viewBox='0 0 16 16'>
+  <path d='M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z'/>
+</svg></a>";
+        echo "<ul class='dropdown-menu' aria-labelledby='dropdownMenuLink'>";
+        echo '<li><a class="dropdown-item" href="#">Editar</a></li >';
+        echo "<li><a href='borrarJugador.php?id=$numero' class='dropdown-item'>Eliminar</a></li>";
 
-
+        echo '</ul>';
+        echo '</div>';
     }
-
-
     echo '</div>';
-
+    echo '<div class="d-flex flex-column">';
+    echo '<div class="d-flex">';
+    echo '<h5 class="mt-1">Posición:</h5>';
+    echo "<img class='mt-2 ms-2 align-self-start' style='width: 2em' src='$imgMostrarPosicion'>";
     echo '</div>';
+    echo '<a href="#" class="btn btn-primary mt-2">Ver Perfil</a>';
+    echo '</div>';
+    echo '</div>';
+    echo '</div>';
+    echo "</div>";
+
 }
-
-
 ?>
