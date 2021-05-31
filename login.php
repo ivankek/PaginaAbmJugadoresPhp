5ebe2e7b1b71b8b29b12b3dd2225b0e93bd5674f
ivankek <ivan.kek@hotmail.com>
2021-05-31 01:40:25 -0300

    Login seguro

--- login.php
+++ login.php
@@ -7,22 +7,26 @@ $usuario = isset($_POST["usuario"]) ? $_POST["usuario"] : "";
 $password = isset($_POST["password"]) ? $_POST["password"] : "";
 
 
-$sql = "SELECT * FROM usuario WHERE usuario = '$usuario'";
+$sql = "SELECT * FROM usuario WHERE usuario = ?;";
 
-$result = $conexion->query($sql);
+$stmt = $conexion->prepare($sql);
+$parametro = $usuario;
+$stmt->bind_param("s", $parametro);
+$stmt->execute();
+$result = $stmt->get_result();
 
-if ($fila = mysqli_fetch_array($result)) {
+if ($fila = $result->fetch_assoc()) {
 
     echo $fila["usuario"];
 
-     if ($fila['pass'] == $password) {
-         session_start();
+    if ($fila['pass'] == $password) {
+        session_start();
 
-         $_SESSION['nombre'] = $usuario;
+        $_SESSION['nombre'] = $usuario;
 
-         header("Location: jugadores.php");
-         exit();
-     }
+        header("Location: jugadores.php");
+        exit();
+    }
 }
 
 header("Location: jugadores.php");
