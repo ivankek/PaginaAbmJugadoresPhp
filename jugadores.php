d765e9e8dd6fb542b423e978a4f6d045429eb1e9
ivankek <ivan.kek@hotmail.com>
2021-06-02 19:52:38 -0300

    Arreglos

--- jugadores.php
+++ jugadores.php
@@ -72,7 +72,7 @@
             <div class="modal-content">
                 <form action="agregarJugador.php" method="POST" enctype="multipart/form-data">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Agregar un pokemon</h5>
+                        <h5 class="modal-title" id="exampleModalLabel">Agregar un jugador</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
